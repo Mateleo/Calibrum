a3a48e04e0d5db0112510c1edbb5d676063a86a7
Mateleo <mateleo78@gmail.com>
2023-05-04 20:13:49 +0200

    🎨 style(nuxt.config.ts): format code and add font weight to Inter font 🚀 perf(player.vue): add different font weights to player name display

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: [
-        '@nuxtjs/tailwindcss',
-        '@nuxtjs/google-fonts'
-      ],
-      googleFonts: {
-        families: {
-          "Inter":true
-        }
-      }
-})
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
+  googleFonts: {
+    families: {
+      Inter: {
+        wght:[100,400,600,700]
+      },
+    },
+  },
+});
--- pages/player/[player].vue
+++ pages/player/[player].vue
@@ -2,5 +2,12 @@
 const route = useRoute()
 </script>
 <template>
-    {{ route.params.player }}
+    <div class="text-4xl font-bold">{{ route.params.player }}
+    </div>
+    <div class="text-4xl font-semibold">{{ route.params.player }}
+    </div>
+    <div class="text-4xl">{{ route.params.player }}
+    </div>
+    <div class="text-4xl font-light">{{ route.params.player }}
+    </div>
 </template>
