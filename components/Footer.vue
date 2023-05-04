3df778ed804cf7f4faa6cb83ae7b274c0e0d3050
Mateleo <mateleo78@gmail.com>
2023-05-04 18:09:00 +0200

    ✨ feat(app.vue): add default layout to the app ✨ feat(components): add Header and Footer components ✨ feat(layouts): add default layout with Header and Footer components

---
+++ components/Footer.vue
@@ -0,0 +1,6 @@
+<script lang=ts setup></script>
+<template>
+    <div>
+        Footer
+    </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <Header></Header>
+        <slot />
+        <Footer></Footer>
+    </div>
+</template>
