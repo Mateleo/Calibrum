4828aa55199fada6a2aa8b7cbbabcde7479a4075
Turdyo <martin.gayet@efrei.net>
2023-05-05 15:25:43 +0200

    🎨 style(Header.vue): change header background color to #22262b for better contrast and readability 🚀 feat(live-games): add live-games page with 4 sections of 2x2 player grids to display live games data

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts"></script>
 <template>
-    <header class="flex bg-[#292E35] shadow-lg items-center sticky top-0 h-[68px]">
+    <header class="flex bg-[#22262b] shadow-lg items-center sticky top-0 h-[68px]">
         <NuxtLink to="/" class="flex items-center px-8">
             <div class="ml-2 w-[48px]">
                 <NuxtImg src="logo.png"></NuxtImg>
@@ -11,11 +11,11 @@
         </NuxtLink>
         <div
             class="max-w-[500px] grow m-auto flex items-center border-2 border-gray-600 p-2 focus-within:outline-none focus-within:border-sky-400 focus-within:border-2 rounded-lg focus-within:shadow-lg focus-within:shadow-black/40">
-            <input placeholder="Search..." class="w-full block bg-[#292E35] focus:outline-none" />
+            <input placeholder="Search..." class="w-full block bg-[#22262b] focus:outline-none" />
             <Icon name="material-symbols:search" size="1.6em"></Icon>
         </div>
         <nav class="flex justify-between px-4 h-full">
-            <NuxtLink to=""
+            <NuxtLink to="/live-games"
                 class="h-full border-b-[6px] hover:border-cyan-300 border-transparent flex items-center transition-colors ease-in grow font-semibold pt-[6px] cursor-pointer">
                 LIVE GAMES (WIP)
             </NuxtLink>
