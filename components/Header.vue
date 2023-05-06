1fa7d898dd7cdb9f8488a89a0d044cfb51199f15
Mateleo <mateleo78@gmail.com>
2023-05-06 19:20:18 +0200

    💄 style(Header.vue): adjust header layout and search bar width 🚀 perf(Header.vue): add max width to search bar and adjust nav link border size

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts"></script>
 <template>
-    <header class="grid grid-cols-5 bg-[#22262b] shadow-lg items-center sticky top-0 h-[68px]">
+    <header class="grid grid-cols-5 gap-8 bg-[#22262b] shadow-lg items-center sticky top-0 h-[68px]">
         <NuxtLink to="/" class="flex items-center px-4">
             <div class="ml-2 w-[48px]">
                 <NuxtImg src="logo.png"></NuxtImg>
@@ -10,13 +10,13 @@
             </div>
         </NuxtLink>
         <div
-            class="col-span-3 max-w-[500px] grow m-auto flex items-center border-2 border-gray-600 p-2 focus-within:outline-none focus-within:border-sky-400 focus-within:border-2 rounded-lg focus-within:shadow-lg focus-within:shadow-black/40">
+            class="col-span-3 w-full m-auto max-w-[500px] flex items-center border-2 border-gray-600 p-2 focus-within:outline-none focus-within:border-sky-400 focus-within:border-2 rounded-lg focus-within:shadow-lg focus-within:shadow-black/40">
             <input placeholder="Search..." class="w-full block bg-[#22262b] focus:outline-none" />
             <Icon name="material-symbols:search" size="1.6em"></Icon>
         </div>
-        <nav class="flex flex-row-reverse">
+        <nav class="flex h-full m-auto mr-0 px-4">
             <NuxtLink to="/live-games"
-                class="border-b-[6px] hover:border-cyan-300 border-transparent flex items-center transition-colors ease-in grow font-semibold pt-[6px] cursor-pointer whitespace-nowrap">
+                class="border-b-[4px] hover:border-cyan-300 border-transparent flex items-center transition-colors ease-in grow font-semibold pt-[6px] cursor-pointer whitespace-nowrap">
                 LIVE GAMES (WIP)
             </NuxtLink>
         </nav>
