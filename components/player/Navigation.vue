4b763e76269da4c54dcc3b6990ad7eab38f99dfd
Turdyo <martin.gayet@efrei.net>
2023-05-09 17:25:55 +0200

    🎨 style(Navigation.vue): change selected variable type to a union type of string literals to improve type safety and readability 🚀 perf(Navigation.vue): remove unnecessary div wrapper element to improve rendering performance 📝 docs(Navigation.vue): add newline at the end of the file to follow best practices

--- components/player/Navigation.vue
+++ components/player/Navigation.vue
@@ -1,14 +1,12 @@
 <script lang=ts setup>
 
-let selected = ref(0)
+let selected = ref<"accounts" | "liveGame" | "badges">("accounts")
 
 </script>
 <template>
-    <div>
-        <nav class="flex h-[40px] gap-2 text-sm">
-            <CommonNavButton :active="selected==0 ? true : false" @click="selected=0">ACCOUNTS</CommonNavButton>
-            <CommonNavButton :active="selected==1 ? true : false" @click="selected=1">LIVE GAME</CommonNavButton>
-            <CommonNavButton :active="selected==2 ? true : false" @click="selected=2">BADGES</CommonNavButton>
-        </nav>
-    </div>
+    <nav class="flex h-[40px] gap-2 text-sm">
+        <CommonNavButton :active="selected == 'accounts'" @click="selected = 'accounts'">ACCOUNTS</CommonNavButton>
+        <CommonNavButton :active="selected == 'liveGame'" @click="selected = 'liveGame'">LIVE GAME</CommonNavButton>
+        <CommonNavButton :active="selected == 'badges'" @click="selected = 'badges'">BADGES</CommonNavButton>
+    </nav>
 </template>
