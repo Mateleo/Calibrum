0cc82892c2e60e315d99cd45d866816e3043861d
Mateleo <mateleo78@gmail.com>
2023-05-09 17:41:41 +0200

    🎨 style(NavButton.vue): add font-medium class to NavButton component 🎨 style(Accounts.vue): add font-medium class to CommonSection component 🎨 style(Navigation.vue): add text-white/90 class to active CommonNavButton component 🎨 style(nuxt.config.ts): add 500 weight to Inter font family

--- components/common/NavButton.vue
+++ components/common/NavButton.vue
@@ -12,7 +12,7 @@ const props: props = withDefaults(
 
 </script>
 <template>
-    <button class="border-b-[4px] hover:border-cyan-300 transition-colors ease-in px-2"
+    <button class="border-b-[4px] hover:border-cyan-300 transition-colors ease-in px-2 font-semibold text-white/80"
         :class="props.active ? 'border-cyan-300' : 'border-transparent'">
         <slot></slot>
     </button>
--- components/player/Navigation.vue
+++ components/player/Navigation.vue
@@ -5,8 +5,8 @@ let selected = ref<"accounts" | "liveGame" | "badges">("accounts")
 </script>
 <template>
     <nav class="flex h-[40px] gap-2 text-sm">
-        <CommonNavButton :active="selected == 'accounts'" @click="selected = 'accounts'">ACCOUNTS</CommonNavButton>
-        <CommonNavButton :active="selected == 'liveGame'" @click="selected = 'liveGame'">LIVE GAME</CommonNavButton>
-        <CommonNavButton :active="selected == 'badges'" @click="selected = 'badges'">BADGES</CommonNavButton>
+        <CommonNavButton :active="selected == 'accounts'" @click="selected = 'accounts'" :class="selected=='accounts' ? 'text-white/90': ''">ACCOUNTS</CommonNavButton>
+        <CommonNavButton :active="selected == 'liveGame'" @click="selected = 'liveGame'" :class="selected=='liveGame' ? 'text-white/90': ''">LIVE GAME</CommonNavButton>
+        <CommonNavButton :active="selected == 'badges'" @click="selected = 'badges'" :class="selected=='badges' ? 'text-white/90': ''">BADGES</CommonNavButton>
     </nav>
 </template>
