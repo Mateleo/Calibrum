33f5ab9653ed095e9da9340be8ae524980239121
Mateleo <mateleo78@gmail.com>
2023-05-09 22:36:17 +0200

    🎨 style(Footer.vue): add hover effect to social media icons and 4esport logo 🎨 style(Header.vue): adjust header layout for responsiveness and add search bar for desktop view

--- components/Footer.vue
+++ components/Footer.vue
@@ -9,19 +9,21 @@
             </div>
             <!-- <div>Copyright © 2023 4eSport Tous droits réservés.</div> -->
             <div class="flex gap-6">
-                <a href="https://www.discord.gg/4eSport" target="-blank">
+                <a href="https://www.discord.gg/4eSport" target="-blank" class="hover:text-white transition-all ease-in">
                     <Icon name="ic:sharp-discord" size="1.6em" cursor="pointer"></Icon>
                 </a>
-                <a href="https://www.instagram.com/4esport_efrei/" target="-blank">
+                <a href="https://www.instagram.com/4esport_efrei/" target="-blank"
+                    class="hover:text-white  transition-all ease-in">
                     <Icon name="mdi:instagram" size="1.6em" cursor="pointer"></Icon>
                 </a>
-                <a href="https://twitter.com/4esport_efrei" target="-blank">
+                <a href="https://twitter.com/4esport_efrei" target="-blank"
+                    class="hover:text-white  transition-all ease-in">
                     <Icon name="mdi:twitter" size="1.6em" cursor="pointer"></Icon>
                 </a>
-                <a href="https://4esport.fr/" target="-blank" class="w-[22px] opacity-50">
+                <a href="https://4esport.fr/" target="-blank"
+                    class="w-[22px] hover:opacity-100 opacity-50  transition-all ease-in">
                     <NuxtImg src="4es_logo.png"></NuxtImg>
                 </a>
             </div>
         </div>
-    </footer>
-</template>
+</footer></template>
--- components/Header.vue
+++ components/Header.vue
@@ -1,20 +1,23 @@
 <script setup lang="ts"></script>
 <template>
-    <header class="grid grid-cols-5 gap-8 bg-[#22262b] shadow-lg shadow-black/30 items-center sticky top-0 h-[68px]">
-        <NuxtLink to="/" class="flex items-center px-4">
-            <div class="ml-2 w-[48px]">
-                <NuxtImg src="logo.png"></NuxtImg>
-            </div>
-            <div class="px-2 flex flex-row-reverse p-2" to="/">
-                <h1 class="hidden lg:block text-4xl font-bold">Calibrum</h1>
-            </div>
-        </NuxtLink>
+    <header
+        class="flex md:grid md:grid-cols-10 gap-4 md:gap-8 bg-[#22262b] shadow-lg shadow-black/30 items-center sticky top-0 h-[68px]">
+        <div class="col-span-3">
+            <NuxtLink to="/" class="flex items-center px-2 md:px-4 border-r-[1px] border-gray-500/50 md:border-transparent pr-4">
+                <div class="ml-2 w-[48px]">
+                    <NuxtImg src="logo.png"></NuxtImg>
+                </div>
+                <div class="px-2 hidden md:flex flex-row-reverse p-2" to="/">
+                    <h1 class="hidden lg:block text-4xl font-bold">Calibrum</h1>
+                </div>
+            </NuxtLink>
+        </div>
         <div
-            class="col-span-3 w-full m-auto max-w-[500px] flex items-center border-2 border-gray-600 p-2 focus-within:outline-none focus-within:border-sky-400 focus-within:border-2 rounded-lg focus-within:shadow-lg focus-within:shadow-black/40">
+            class="hidden lg:col-span-4 w-full m-auto max-w-[500px] md:flex items-center border-2 border-gray-600 p-2 focus-within:outline-none focus-within:border-sky-400 focus-within:border-2 rounded-lg focus-within:shadow-lg focus-within:shadow-black/40">
             <input placeholder="Search..." class="w-full block bg-[#22262b] focus:outline-none" />
             <Icon name="material-symbols:search" size="1.6em"></Icon>
         </div>
-        <nav class="flex h-full m-auto mr-0 px-4">
+        <nav class="flex md:h-full md:m-auto mr-0 px-4 text-white/95 col-span-3">
             <NuxtLink to="/live-games"
                 class="border-b-[4px] hover:border-cyan-300 border-transparent flex items-center transition-colors ease-in grow font-semibold pt-[6px] whitespace-nowrap">
                 LIVE GAMES (WIP)
