3823768edc254f181081ece6ec82f2d41aee5592
Turdyo <martin.gayet@efrei.net>
2023-05-11 20:44:26 +0200

    🚀 feat([player].ts): add lpUpdates to accounts returned by getPlayerByName to provide more information about accounts.

--- server/api/player/[player].ts
+++ server/api/player/[player].ts
@@ -1,4 +1,5 @@
 import { getAccountsByPlayer } from "~/server/helpers/accounts/accounts"
+import { getLpUpdateByAccount } from "~/server/helpers/lp_updates/lp_updates"
 import { getPlayerByName } from "~/server/helpers/players/players"
 
 export default defineEventHandler(async (event) => {
@@ -16,10 +17,19 @@ export default defineEventHandler(async (event) => {
         return { error: `player ${params.player} not found` }
     }
 
-    var accounts = await getAccountsByPlayer(player.discordId)
+    const accounts = await getAccountsByPlayer(player.discordId)
+
+    const accountsWithLpUpdates = await Promise.all(accounts.map(async (account) => {
+        const lpUpdates = await getLpUpdateByAccount(account.id)
+        return {
+            ...account,
+            lpUpdates: lpUpdates
+        }
+    }))
+
 
     return {
         ...player,
-        accounts: accounts
+        accounts: accountsWithLpUpdates
     }
 })
