ab4b1ee70d0ac3f16c6b0588460a3ea12b2b71de
Mateleo <mateleo78@gmail.com>
2023-05-13 18:07:32 +0200

    🎨 style(Rank.vue): add fade-in animation to rank section for better user experience 🚀 chore(package.json): add animate.css dependency for better animation support

--- components/player/Rank.vue
+++ components/player/Rank.vue
@@ -32,15 +32,30 @@ const { title, losses, wins, lpUpdate } = toRefs(props)
 <template>
     <div>
         <CommonTitleSection :title="title" :padding="false">
-            <div class="flex justify-between items-center">
-                <NuxtImg :src="`img/emblems/Emblem_${lpUpdate.tier ?? 'IRON'}.png`" class="object-cover w-[155px] h-[95px] opacity-60"></NuxtImg>
-                <div class="text-sm text-right p-4 leading-4 flex flex-col justify-center">
-                    <p class="font-semibold">{{ lpUpdate.rank }} {{ lpUpdate.tier }}</p>
-                    <p class="font-semibold">{{ lpUpdate.LP }} LP</p>
-                    <p>{{ wins }}/{{ losses }} ({{ Math.floor(wins / (losses + wins) * 1000) / 10 }}%)</p>
-                    <p class="text-white/70">{{ dayjs(lpUpdate.date).format("DD/MM") }}</p>
-                </div>
+            <div class="w-[300px] h-[95px] overflow-hidden">
+                <Transition name="fade">
+                    <div class="flex justify-between items-center" :key="lpUpdate.date.toString()">
+                        <NuxtImg :src="`img/emblems/Emblem_${lpUpdate.tier ?? 'IRON'}.png`"
+                            class="object-cover w-[155px] h-[95px] opacity-60"></NuxtImg>
+                        <div class="text-sm text-right p-4 leading-4 flex flex-col justify-center w-[140px]">
+                            <p class="font-semibold">{{ lpUpdate.rank }} {{ lpUpdate.tier }}</p>
+                            <p class="font-semibold">{{ lpUpdate.LP }} LP</p>
+                            <p>{{ wins }}/{{ losses }} ({{ Math.floor(wins / (losses + wins) * 1000) / 10 }}%)</p>
+                            <p class="text-white/70">{{ dayjs(lpUpdate.date).format("DD/MM") }}</p>
+                        </div>
+                    </div>
+                </Transition>
             </div>
         </CommonTitleSection>
     </div>
 </template>
+<style scoped>
+.fade-enter-active{
+    transition: all 0.8s;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+</style>
--- pages/player/[player].vue
+++ pages/player/[player].vue
@@ -62,3 +62,4 @@ useSeoMeta({
         </div>
     </div>
 </template>
+
