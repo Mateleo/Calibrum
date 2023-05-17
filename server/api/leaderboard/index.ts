f923eacb346ce8e069f256a7ee4f0446391e0026
Mateleo <mateleo78@gmail.com>
2023-05-17 22:49:54 +0200

    🐛 fix(leaderboard): sort players by LPC in descending order to show the highest scores first ✨ feat(leaderboard): import Account and Player types from @prisma/client to improve type safety

--- server/api/leaderboard/index.ts
+++ server/api/leaderboard/index.ts
@@ -1,8 +1,8 @@
+import { Account, Player } from "@prisma/client";
 import { getPlayers } from "~/server/utils/players/players";
 
 export default defineEventHandler(async (event) => {
-    const players = await getPlayers()
+  const players = await getPlayers();
 
-    return players
-  });
-  
+  return players.sort((a, b) => (b.Account[0].LPC ?? 0) - (a.Account[0].LPC ?? 0));
+});
