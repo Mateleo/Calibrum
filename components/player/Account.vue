226720921cc63a267d1188a718c2ee9bea615a5e
Turdyo <martin.gayet@efrei.net>
2023-05-23 17:49:07 +0200

    ♻️ refactor(Account.vue): remove unused variables and simplify code by removing intermediate variables and using reduce to calculate peakEloUpdate 🐛 fix(Account.vue): fix PlayerRank component props by removing unused lastUpdate variable and using account.lpUpdates.at(0) instead of lastUpdate variable. Also, fix peakEloUpdate calculation by using reduce function.

--- components/player/Account.vue
+++ components/player/Account.vue
@@ -8,17 +8,15 @@ export interface AccountProps {
 const props = defineProps<AccountProps>()
 const { account } = toRefs(props)
 
-const lastUpdate = account.value.lpUpdates.at(0)
-const peakEloUpdate = account.value.lpUpdates.reduce((peakEloUpdate, currentUpdate) => (currentUpdate.LPC > peakEloUpdate.LPC ? currentUpdate : peakEloUpdate))
-
 </script>
 <template>
     <div class="flex mt-4 gap-8">
         <div class="flex flex-col shrink-0">
-            <PlayerRank :lpUpdate="lastUpdate" :title="'Current Rank'" :wins="account.wins ?? 0"
+            <PlayerRank :lpUpdate="account.lpUpdates.at(0)" :title="'Current Rank'" :wins="account.wins ?? 0"
                 :losses="account.losses ?? 0" />
-            <PlayerRank :lpUpdate="peakEloUpdate" :title="'Peak Rank'" :wins="account.wins ?? 0"
-                :losses="account.losses ?? 0" class="mt-8" />
+            <PlayerRank
+                :lpUpdate="account.lpUpdates.reduce((peakEloUpdate, currentUpdate) => (currentUpdate.LPC > peakEloUpdate.LPC ? currentUpdate : peakEloUpdate))"
+                :title="'Peak Rank'" :wins="account.wins ?? 0" :losses="account.losses ?? 0" class="mt-8" />
         </div>
         <div class="flex flex-col w-full">
             <CommonTitleSection title="Rank History" class="h-full">
