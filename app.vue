d1f0d17fca0a275beaecd6f1b48a2dc0f4af9046
Mateleo <mateleo78@gmail.com>
2023-08-14 16:03:24 +0200

    typo

--- app.vue
+++ app.vue
@@ -1,3 +1,33 @@
+<script lang="ts" setup>
+useHead({
+  title: "Calibrum 🌠",
+  meta: [
+    {
+      name: "description",
+      content: "Calibrum : La nouvelle référence pour les joueurs LoL. de 4eSport"
+    },
+    { property: "og:type", content: "website" },
+    { property: "og:url", content: "https://calibrum.4esport.fr/" },
+    { property: "og:title", content: "Phoenix Development" },
+    {
+      property: "og:description",
+      content: "Calibrum : La nouvelle référence pour les joueurs LoL. de 4eSport."
+    },
+    { name: "twitter:title", content: "Calibrum 🌠" },
+    {
+      name: "twitter:description",
+      content: "Calibrum : La nouvelle référence pour les joueurs LoL. de 4eSport."
+    }
+  ],
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/favicon.ico"
+    }
+  ]
+})
+</script>
 <template>
   <div>
     <NuxtLayout>
