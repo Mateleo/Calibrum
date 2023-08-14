f6e7dd2c061b5c25aa13bc0dd3d396bc35d9cb87
Mateleo <mateleo78@gmail.com>
2023-08-14 15:17:03 +0200

    🎉 feat(Modal.vue): add Modal component to display a button with project information and hide functionality 🔧 chore(index.vue): fix formatting issue in index.vue file

---
+++ components/main/Modal.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+const visible = ref(false)
+
+const hideButton = () => {
+  visible.value = false
+  localStorage.setItem("buttonHidden", "true")
+}
+
+// Check if the button was hidden before when the component mounts
+onBeforeMount(() => {
+  const buttonHidden = localStorage.getItem("buttonHidden")
+  if (buttonHidden === "true") {
+    visible.value = true
+  }
+})
+</script>
+<template>
+  <div>
+    <button
+      v-if="visible"
+      @click="hideButton"
+      class="font-semibold mt-12 rounded-md border-[1px] border-sky-400 bg-white/5 p-4 text-left text-sky-500 transition-colors ease-in hover:bg-sky-400/10"
+    >
+      Voici notre dernier projet ! Calibrum est une récriture complète du Lolpros de 4eSport. Tout le monde est le
+      bienvenue, alors n'hésitez pas à vous incrire via notre bot sur le Discord de l'association <a class="underline" href="https://discord.gg/YsA7Md2">ici</a>.
+      <p class="italic text-sm">@Turdyo & @Mateleo</p>
+    </button>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,12 @@
 <script lang="ts" setup></script>
 <template>
-  <div class="flex flex-col md:flex-row max-w-[1500px] m-auto mt-12 gap-4 md:gap-12 w-[98%] sm:w-[95%]">
+  <div class="flex flex-col max-w-[1500px] m-auto w-[98%] sm:w-[95%]">
+    <LazyMainModal class=""></LazyMainModal>
+    <div class="flex flex-col md:flex-row gap-4 md:gap-12 mt-12">
     <MainLadder class="h-min grow" />
     <div class="flex flex-col gap-4 md:w-[40%]">
       <LazyMainPlayersOfTheDay/>
       <LazyMainLastGames />
     </div>
-  </div>
+  </div></div>
 </template>
