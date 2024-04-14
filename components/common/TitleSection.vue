c80e93d948aaa97e88347506648d868461683074
Mateleo <mateleo78@gmail.com>
2024-04-14 22:38:00 +0200

    opgg fix

--- components/common/TitleSection.vue
+++ components/common/TitleSection.vue
@@ -17,7 +17,7 @@ const { padding, title, color,player } = toRefs(props);
   <div class="flex flex-col">
     <div class="flex justify-between mb-2 items-center">
       <CommonTitle class="">{{ title }}</CommonTitle>
-      <a v-if="player" target="_blank" :href="`https://www.op.gg/summoners/euw/${player}`" class="hover:bg-sky-400 border-sky-400/70 border font-bold text-sm p-1 rounded-sm">OP.GG</a>
+      <a v-if="player" target="_blank" :href="`https://www.op.gg/summoners/euw/${player.split('#')[0]}`" class="hover:bg-sky-400 border-sky-400/70 border font-bold text-sm p-1 rounded-sm">OP.GG</a>
     </div>
     <CommonSection class="h-full rounded-lg" :padding="padding" :color="color">
       <slot></slot>
