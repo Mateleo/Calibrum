41e7c56643b465c01667e2c66ebe243261a2c029
Turdyo <martin.gayet@efrei.net>
2024-07-29 23:07:37 +0200

    fix opgg links

--- components/common/TitleSection.vue
+++ components/common/TitleSection.vue
@@ -1,23 +1,25 @@
 <script setup lang="ts">
 interface Props {
-  padding?: boolean;
-  title: string;
-  color?: string;
-  player?: string
+	padding?: boolean;
+	title: string;
+	color?: string;
+	player?: string;
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  padding: true,
-  color: "#22262b",
+	padding: true,
+	color: "#22262b",
 });
 
-const { padding, title, color,player } = toRefs(props);
+const { padding, title, color, player } = toRefs(props);
 </script>
 <template>
   <div class="flex flex-col">
     <div class="flex justify-between mb-2 items-center">
       <CommonTitle class="">{{ title }}</CommonTitle>
-      <a v-if="player" target="_blank" :href="`https://www.op.gg/summoners/euw/${player.split('#')[0]}`" class="hover:bg-sky-400 border-sky-400/70 border font-bold text-sm p-1 rounded-sm">OP.GG</a>
+      <a v-if="player" target="_blank"
+        :href="`https://www.op.gg/summoners/euw/${player.split('#').at(0)}-${player.split('#').at(1)}`"
+        class="hover:bg-sky-400 border-sky-400/70 border font-bold text-sm p-1 rounded-sm">OP.GG</a>
     </div>
     <CommonSection class="h-full rounded-lg" :padding="padding" :color="color">
       <slot></slot>
