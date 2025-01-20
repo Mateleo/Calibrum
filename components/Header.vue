e9db6ce5118da9bb696d0443b2674451e29e72d5
Mateleo <mateleo78@gmail.com>
2025-01-20 22:41:19 +0100

    💡 (Header.vue): update Header component layout and styling for better user experience; add responsive design using flexbox; improve search bar UI with placeholder text, icon, and results display
    💡 (SearchBar.vue): refactor SearchBar component to use relative positioning and absolute positioning for input field and results container; adjust input size, placeholder text, and icon size for consistency

--- components/Header.vue
+++ components/Header.vue
@@ -1,28 +1,30 @@
 <script setup lang="ts"></script>
 <template>
-  <header class="sticky top-0 z-10 h-[64px] gap-4 bg-[#22262b] py-2 shadow-lg shadow-black/30 md:gap-8">
-    <div class="m-auto flex max-w-[2000px] justify-center md:grid md:grid-cols-3 lg:grid-cols-10">
-      <div class="hidden md:block lg:col-span-3">
+  <header
+    class="z-10 m-auto mt-4 h-[64px] w-[95%] max-w-[1500px] gap-4 rounded-xl bg-[#22262b] py-2 shadow-lg shadow-black/30 md:gap-8"
+  >
+    <div class="flex h-full justify-center md:grid md:grid-cols-3 lg:grid-cols-10">
+      <div class="hidden md:flex lg:col-span-3">
         <NuxtLink
           to="/"
           class="flex items-center border-r-[1px] border-gray-500/50 px-2 pr-4 md:border-transparent md:px-4"
         >
-          <div class="ml-2 w-[48px]">
+          <div class="ml-2 w-[42px]">
             <NuxtImg src="img/logo.png" sizes="64px" format="webp" quality="70"></NuxtImg>
           </div>
           <div class="hidden flex-row-reverse px-2 md:flex" to="/">
-            <h1 class="hidden text-4xl font-bold lg:block">Calibrum</h1>
+            <h1 class="hidden text-3xl font-bold lg:block">Calibrum</h1>
           </div>
         </NuxtLink>
       </div>
       <HeaderSearchBar class=""></HeaderSearchBar>
-      <nav class="mt-2 px-4 text-white/80 md:ml-auto md:text-white/95 lg:col-span-3">
-        <!-- <NuxtLink
+      <nav class="ml-auto flex flex-col px-4 lg:col-span-3">
+        <NuxtLink
           to="/live-games"
-          class="text-xs sm:text-base flex grow items-center whitespace-nowrap border-b-[4px] border-transparent pt-[6px] font-semibold transition-colors ease-in hover:border-cyan-300"
+          class="flex grow items-center whitespace-nowrap border-b-[4px] border-transparent pt-[6px] text-xs font-semibold transition-colors ease-in hover:border-cyan-300 sm:text-base"
         >
-          LIVE GAMES (WIP)
-        </NuxtLink> -->
+          Blog (WIP)
+        </NuxtLink>
       </nav>
     </div>
   </header>
