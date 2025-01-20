f83ace4463846fd3705d5ca4cebdf10864a2b5fc
Mateleo <mateleo78@gmail.com>
2025-01-20 23:50:58 +0100

    💡 (Header.vue): Update Header component layout and style for better responsiveness and consistency. 💡 (SearchBar.vue): Modify SearchBar component to use flex-col layout, improve search functionality and styling for better user experience.

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts"></script>
 <template>
   <header
-    class="z-10 m-auto mt-4 h-[64px] w-[95%] max-w-[1500px] gap-4 rounded-xl bg-[#22262b] py-2 shadow-lg shadow-black/30 md:gap-8"
+    class="z-10 m-auto mt-4 h-[64px] w-[98%] max-w-[1500px] gap-4 rounded-md bg-[#22262b] py-2 shadow-lg shadow-black/30 md:w-[95%] md:gap-8 md:rounded-xl"
   >
-    <div class="flex h-full justify-center md:grid md:grid-cols-3 lg:grid-cols-10">
+    <div class="flex h-full justify-between gap-4 px-2 md:grid md:justify-center md:px-0 lg:grid-cols-10">
       <div class="hidden md:flex lg:col-span-3">
         <NuxtLink
           to="/"
@@ -17,8 +17,8 @@
           </div>
         </NuxtLink>
       </div>
-      <HeaderSearchBar class=""></HeaderSearchBar>
-      <nav class="ml-auto flex flex-col px-4 lg:col-span-3">
+      <HeaderSearchBar class="grow"></HeaderSearchBar>
+      <nav class="flex flex-col md:ml-auto md:px-4 lg:col-span-3">
         <NuxtLink
           to="/live-games"
           class="flex grow items-center whitespace-nowrap border-b-[4px] border-transparent pt-[6px] text-xs font-semibold transition-colors ease-in hover:border-cyan-300 sm:text-base"
--- components/Header/SearchBar.vue
+++ components/Header/SearchBar.vue
@@ -47,54 +47,56 @@ watch(isOutside, () => {
 
 <template>
   <div ref="target" class="relative flex justify-center lg:col-span-4">
-    <div class="absolute top-[5px] w-full">
-      <div
-        class="m-auto w-full max-w-[350px] rounded-lg border-2 border-gray-600 p-2 focus-within:border-2 focus-within:border-sky-400 focus-within:shadow-lg focus-within:shadow-black/40 focus-within:outline-none"
-        :class="results.length > 0 ? 'bg-[#22262b]/70 shadow-md shadow-black/60 backdrop-blur-[6px]' : ''"
-      >
-        <div class="flex items-center">
-          <input
-            ref="target2"
-            @keyup.enter="results.length > 0 ? (navigateTo(`/player/${results.at(0)?.name}`), flush()) : ''"
-            @input="fuzzySearch"
-            v-model="search"
-            placeholder="Search..."
-            class="block w-full bg-[#22262b] text-sm font-semibold placeholder:text-sm placeholder:font-normal focus:outline-none"
-          />
-          <Icon name="material-symbols:search" size="1.2em"></Icon>
-        </div>
-        <div v-if="players && results.length > 0" class="mt-2">
-          <button
-            @click="(navigateTo(`/player/${player.name}`), flush())"
-            v-for="(player, index) in results.slice(0, 4)"
-            class="flex w-full rounded-md p-1 focus-within:bg-white/5 hover:bg-white/5"
-          >
-            <div class="flex items-center py-2">
-              <NuxtImg
-                sizes="50px"
-                quality="80"
-                format="webp"
-                class="mr-5 hidden w-[50px] rounded-lg sm:block"
-                :src="player.Account[0].profileIcon"
-                alt=""
-              />
-              <div>
-                {{ player.Account[0].name }}
-              </div>
-            </div>
-            <div class="flex max-w-[300px] grow items-center justify-end">
-              <!-- <div class="flex flex-col justify-center">
-                <img
-                  class="m-auto h-[50px] w-[90px] object-cover"
-                  :src="`img/new_emblems/${player.Account[0].tier ?? 'IRON'}.png`"
+    <div class="absolute flex h-full w-full flex-col justify-center">
+      <div class="h-[42px]">
+        <div
+          class="relative m-auto w-full max-w-[350px] rounded-lg border-2 border-gray-600 p-2 focus-within:border-2 focus-within:border-sky-400 focus-within:shadow-lg focus-within:shadow-black/40 focus-within:outline-none"
+          :class="results.length > 0 ? 'bg-[#22262b]/70 shadow-md shadow-black/60 backdrop-blur-[6px]' : ''"
+        >
+          <div class="flex items-center">
+            <input
+              ref="target2"
+              @keyup.enter="results.length > 0 ? (navigateTo(`/player/${results.at(0)?.name}`), flush()) : ''"
+              @input="fuzzySearch"
+              v-model="search"
+              placeholder="Search..."
+              class="block w-full bg-[#22262b] text-sm font-semibold placeholder:text-sm placeholder:font-normal focus:outline-none"
+            />
+            <Icon name="material-symbols:search" size="1.2em"></Icon>
+          </div>
+          <div v-if="players && results.length > 0" class="mt-2">
+            <button
+              @click="(navigateTo(`/player/${player.name}`), flush())"
+              v-for="(player, index) in results.slice(0, 4)"
+              class="flex w-full rounded-md p-1 focus-within:bg-white/5 hover:bg-white/5"
+            >
+              <div class="flex items-center py-2">
+                <NuxtImg
+                  sizes="50px"
+                  quality="80"
+                  format="webp"
+                  class="mr-5 hidden w-[50px] rounded-lg sm:block"
+                  :src="player.Account[0].profileIcon"
                   alt=""
                 />
-                <p class="text-md text-center font-semibold leading-none">
-                  {{ player.Account[0].rank }} - {{ player.Account[0].LP }}
-                </p>
-              </div> -->
-            </div>
-          </button>
+                <div>
+                  {{ player.Account[0].name }}
+                </div>
+              </div>
+              <div class="flex max-w-[300px] grow items-center justify-end">
+                <!-- <div class="flex flex-col justify-center">
+                  <img
+                    class="m-auto h-[50px] w-[90px] object-cover"
+                    :src="`img/new_emblems/${player.Account[0].tier ?? 'IRON'}.png`"
+                    alt=""
+                  />
+                  <p class="text-md text-center font-semibold leading-none">
+                    {{ player.Account[0].rank }} - {{ player.Account[0].LP }}
+                  </p>
+                </div> -->
+              </div>
+            </button>
+          </div>
         </div>
       </div>
     </div>
