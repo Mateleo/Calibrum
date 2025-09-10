8e59365a6d43db6dafaf5e03f902f89b6eb18bc4
Turdyo <martin.gayet@efrei.net>
2025-09-10 23:17:25 +0200

    make logo appear on mobile too

--- components/Header.vue
+++ components/Header.vue
@@ -4,12 +4,9 @@
     class="z-10 m-auto mt-4 h-[64px] w-[94%] max-w-[1500px] gap-4 rounded-md bg-[#22262b] py-2 shadow-lg shadow-black/30 md:gap-8 md:rounded-xl"
   >
     <div class="flex h-full justify-between gap-4 px-2 md:grid md:grid-cols-10 md:justify-center md:px-0">
-      <div class="hidden md:col-span-3 md:flex">
-        <NuxtLink
-          to="/"
-          class="flex items-center border-r-[1px] border-gray-500/50 px-2 pr-4 md:border-transparent md:px-4"
-        >
-          <div class="ml-2 w-[42px]">
+      <div class="flex md:col-span-3">
+        <NuxtLink to="/" class="flex items-center border-r-[1px] border-gray-500/50 pr-2 md:border-transparent md:px-4">
+          <div class="w-[42px] md:ml-2">
             <NuxtImg src="img/logo.png" sizes="64px" format="webp" quality="70"></NuxtImg>
           </div>
           <div class="hidden flex-row-reverse px-2 md:flex" to="/">
